8313635dc407cf3f12fec56ad7e843db49f897f6
Marko Knezevic <markoknezevic75@gmail.com>
2020-11-02 23:06:35 +0100

    Add Repositories to UnitOfWork

--- FireAlarm/FireAlarm.DataAccessLayer/UnitOfWork/IUnitOfWork.cs
+++ FireAlarm/FireAlarm.DataAccessLayer/UnitOfWork/IUnitOfWork.cs
@@ -1,18 +1,17 @@
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using ExerciseTracker.DataAccessLayer.Repositories.Exercises;
-using ExerciseTracker.DataAccessLayer.Repositories.ExerciseWorkouts;
-using ExerciseTracker.DataAccessLayer.Repositories.Statuses;
-using ExerciseTracker.DataAccessLayer.Repositories.Users;
-using ExerciseTracker.DataAccessLayer.Repositories.UserWeights;
-using ExerciseTracker.DataAccessLayer.Repositories.WorkoutRecords;
-using ExerciseTracker.DataAccessLayer.Repositories.Workouts;
+using FireAlarm.DataAccessLayer.Repositories.Alarms;
+using FireAlarm.DataAccessLayer.Repositories.Sensors;
+using FireAlarm.DataAccessLayer.Repositories.Statuses;
+using FireAlarm.DataAccessLayer.Repositories.Temperatures;
 
 namespace FireAlarm.DataAccessLayer.UnitOfWork
 {
     public interface IUnitOfWork
     {
-        IExercisesRepository ExercisesRepository { get; }
+        IStatusesRepository StatusesRepository { get; }
+        ISensorsRepository SensorsRepository { get; }
+        IAlarmsRepository AlarmsRepository { get; }
+        ITemperaturesRepository TemperaturesRepository { get; }
 
         Task SaveChangesAsync();
 
--- FireAlarm/FireAlarm.DataAccessLayer/UnitOfWork/UnitOfWork.cs
+++ FireAlarm/FireAlarm.DataAccessLayer/UnitOfWork/UnitOfWork.cs
@@ -1,6 +1,9 @@
 using System.Threading.Tasks;
-using ExerciseTracker.DataAccessLayer.Repositories.Exercises;
 using FireAlarm.Data;
+using FireAlarm.DataAccessLayer.Repositories.Alarms;
+using FireAlarm.DataAccessLayer.Repositories.Sensors;
+using FireAlarm.DataAccessLayer.Repositories.Statuses;
+using FireAlarm.DataAccessLayer.Repositories.Temperatures;
 
 namespace FireAlarm.DataAccessLayer.UnitOfWork
 {
@@ -14,10 +17,26 @@ namespace FireAlarm.DataAccessLayer.UnitOfWork
         }
 
 
-        private IExercisesRepository _exercisesRepository;
-        public IExercisesRepository ExercisesRepository => this._exercisesRepository ??
-                                                           (this._exercisesRepository =
-                                                               new ExercisesRepository(_fireAlarmDbContext));
+        private IStatusesRepository _statusesRepository;
+        public IStatusesRepository StatusesRepository => this._statusesRepository ??
+                                                          (this._statusesRepository =
+                                                              new StatusesRepository(_fireAlarmDbContext));
+        
+        private IAlarmsRepository _alarmsRepository;
+        public IAlarmsRepository AlarmsRepository => this._alarmsRepository ??
+                                                          (this._alarmsRepository =
+                                                              new AlarmsRepository(_fireAlarmDbContext));
+        
+        private ITemperaturesRepository _temperaturesRepository;
+        public ITemperaturesRepository TemperaturesRepository => this._temperaturesRepository ??
+                                                           (this._temperaturesRepository =
+                                                               new TemperaturesRepository(_fireAlarmDbContext));
+        
+        
+        private ISensorsRepository _sensorsRepository;
+        public ISensorsRepository SensorsRepository => this._sensorsRepository ??
+                                                                 (this._sensorsRepository =
+                                                                     new SensorsRepository(_fireAlarmDbContext));
         
         public Task SaveChangesAsync()
         {
